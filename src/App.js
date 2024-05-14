33a1611aa9a4780acb84eaec5b925da1b83380f9
tokaashraf1 <122158105+tokaashraf1@users.noreply.github.com>
2024-05-14 03:51:35 +0300

    package.json errors solved

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 
 import './App.css';
-<<<<<<< HEAD
-=======
+
 import Header from "./Components/Header/header"
 import Table from './Components/Table/Table';
 import DUMMYY from './Components/DUMMYY';
->>>>>>> master
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
     
-=======
 {/* <Header/> */}
 {/* <Table/> */}
 <DUMMYY/>
->>>>>>> master
     </div>
   );
 }
