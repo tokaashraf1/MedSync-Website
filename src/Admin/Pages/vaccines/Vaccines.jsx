999e0e38c682715748e01441d1b9ea45864dc833
tokaashraf1 <122158105+tokaashraf1@users.noreply.github.com>
2024-05-18 18:17:56 +0300

    vaccines lookup

---
+++ src/Admin/Pages/vaccines/Vaccines.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import  { useState, useEffect } from 'react';
+import Table from "../../../Components/Table/Table"
+import Sidebar from '../Sidebar';
+import Footer from "../../../Components/Footer/Footer";
+import API_ENDPOINT from '../../../utils/constants';
+import Header from '../../../Components/Header/header';
+function Vaccines() {
+  document.body.classList.add("patient-body");
+  const [tableInfo, setTableInfo] = useState({
+    columns: [],
+    rows: [],
+    filteredData: [],
+    creatEndpoint: `${API_ENDPOINT}/api/admin/create/vaccine`,
+    updateEndpoint:`${API_ENDPOINT}/api/admin/update/vaccine/`,
+    deleteEndpoint: `${API_ENDPOINT}/api/admin/delete/vaccine/`,
+    request:false,
+    Adminrequest:false,
+    location: "/vaccines",
+  });
+
+  useEffect(() => {
+    fetch(`${API_ENDPOINT}/api/admin/get/all/vaccines`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.length > 0) {
+          // Extracting specific properties from the user object
+          const modifiedData = data.map(({ arabic_name,english_name,id}) => ({
+            english_name,
+            arabic_name,
+            id,
+      
+          }));
+          setTableInfo({
+            ...tableInfo,
+            columns: Object.keys(modifiedData[0]),
+            rows: modifiedData,
+            filteredData: modifiedData,
+          });
+
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  const handleSearch = (filteredData) => {
+    setTableInfo({
+      ...tableInfo,
+      filteredData: filteredData,
+    });
+  };
+
+  return (
+    <div>
+        <Header initialScrolled ={true} initialEnableScroll ={false} page="admin" />
+  
+      <Sidebar/>
+      <Table tableInfo={tableInfo} handleSearch={handleSearch} />
+      <Footer/>
+    </div>
+  )
+}
+
+export default Vaccines
