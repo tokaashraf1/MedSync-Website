d4ad832217c932b6f8138f31f7b562a8e1eab90d
tokaashraf1 <122158105+tokaashraf1@users.noreply.github.com>
2024-05-18 17:19:31 +0300

    footer in treatments lookup

---
+++ src/Admin/Pages/Treatments/Treatments.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import  { useState, useEffect } from 'react';
+import axios from 'axios';
+import Table from "../../components/Table/Table"
+import Search from "../../components/Search/Search"
+import Sidebar from '../../components/Sidebar/Sidebar';
+import Footer from "../../../Components/Footer/Footer";
+import API_ENDPOINT from '../../hooks/constants';
+import Header from '../../components/Header/Header';
+function Treatments() {
+
+  document.body.classList.add("patient-body");
+  const [tableInfo, setTableInfo] = useState({
+    columns: [],
+    rows: [],
+    filteredData: [],
+    creatEndpoint: `${API_ENDPOINT}/api/admin/create/treatment`,
+    updateEndpoint:`${API_ENDPOINT}/api/admin/update/treatment/`,
+    deleteEndpoint:`${API_ENDPOINT}/api/admin/delete/treatment/`,
+    request:false,
+    Adminrequest:false,
+    location: "/treatments",
+  });
+
+
+
+  useEffect(() => {
+    fetch(`${API_ENDPOINT}/api/admin/get/all/treatments`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.length > 0) {
+          // Extracting specific properties from the user object
+          const modifiedData = data.map(({ name,id}) => ({
+            name,
+            id,
+      
+          }));
+          setTableInfo({
+            ...tableInfo,
+            columns: Object.keys(modifiedData[0]),
+            rows: modifiedData,
+            filteredData: modifiedData,
+          });
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  
+    const handleSearch = (filteredData) => {
+      setTableInfo({
+        ...tableInfo,
+        filteredData: filteredData,
+      });
+  };
+
+
+
+  return (
+    <div>
+      <Header initialScrolled ={true} initialEnableScroll ={false} page="admin" />
+  
+      <Sidebar/>
+      <Table tableInfo={tableInfo} handleSearch={handleSearch} />
+    <Footer/>
+    </div>
+  )
+}
+
+export default Treatments
