9a81ebf41d946951188e3db08d39b6ab876b4bb8
tokaashraf1 <122158105+tokaashraf1@users.noreply.github.com>
2024-05-21 03:54:51 +0300

    remove animation

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,3 @@
-const API_ENDPOINT = 'http://localhost:8000';
+// const API_ENDPOINT = 'http://localhost:8000';
+const API_ENDPOINT = 'http://medsyncbackend.smartwaveeg.com:8080';
 export default API_ENDPOINT;
