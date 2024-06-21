fe9c6937d4852a16f640d1a6c3a962901ccaf814
tokaashraf1 <122158105+tokaashraf1@users.noreply.github.com>
2024-06-22 00:07:59 +0300

    last commit

--- src/Admin/Pages/symptoms/Symptoms.jsx
+++ src/Admin/Pages/symptoms/Symptoms.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import  { useState, useEffect } from 'react';
 import Table from "../../../Components/Table/Table"
 import Sidebar from '../Sidebar/Sidebar';
 import Footer from "../../../Components/Footer/Footer";
 import API_ENDPOINT from '../../../utils/constants';
 import Header from '../../../Components/Header/header';
+import { AdminContext } from "../../../Contexts/AdminProvider";
 function Symptoms() {
-
+  const { updateFlag } = useContext(AdminContext);
   document.body.classList.add("admin-pages");
   const [tableInfo, setTableInfo] = useState({
     columns: [],
@@ -42,7 +43,7 @@ function Symptoms() {
       .catch((error) => {
         console.error('Error fetching data:', error);
       });
-  }, []);
+  }, [updateFlag]);
 
   const handleSearch = (filteredData) => {
     setTableInfo({
