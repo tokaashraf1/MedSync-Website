573992c58b5f47fb19c2c852840652fb64fefe20
tokaashraf1 <122158105+tokaashraf1@users.noreply.github.com>
2024-06-22 00:09:17 +0300

    last commit

--- src/Admin/Pages/Treatments/Treatments.jsx
+++ src/Admin/Pages/Treatments/Treatments.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import  { useState, useEffect } from 'react';
 import Table from "../../../Components/Table/Table"
 import Sidebar from '../Sidebar/Sidebar';
 import Footer from "../../../Components/Footer/Footer";
 import API_ENDPOINT from '../../../utils/constants';
 import Header from '../../../Components/Header/header';
+import { AdminContext } from "../../../Contexts/AdminProvider";
 function Treatments() {
   document.body.classList.add("admin-pages");
+  const { updateFlag } = useContext(AdminContext);
   const [tableInfo, setTableInfo] = useState({
     columns: [],
     rows: [],
@@ -40,7 +42,7 @@ function Treatments() {
       .catch((error) => {
         console.error('Error fetching data:', error);
       });
-  }, []);
+  }, [updateFlag]);
 
   
     const handleSearch = (filteredData) => {
