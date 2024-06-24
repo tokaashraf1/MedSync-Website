23097f71eb7060fad579779bfa39cc960db5698b
tokaashraf1 <122158105+tokaashraf1@users.noreply.github.com>
2024-06-25 01:16:30 +0300

    last commit

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
 // const API_ENDPOINT = 'http://localhost:8000';
-const API_ENDPOINT = 'https://medsyncbackend.smartwaveeg.com';
+const API_ENDPOINT = 'http://medsyncbackend.smartwaveeg.com:8080';
 export default API_ENDPOINT;
