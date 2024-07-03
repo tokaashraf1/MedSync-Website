1551c4080d02c7707a1508944f799be7e0d80e08
tokaashraf1 <122158105+tokaashraf1@users.noreply.github.com>
2024-07-04 01:10:24 +0300

    ai commit

--- src/Doctor/Pages/homepage/PatientsList.jsx
+++ src/Doctor/Pages/homepage/PatientsList.jsx
@@ -1,6 +1,11 @@
 import React, { useContext, useState } from "react";
 import { ClinicsContext } from "../../../Contexts/ClinicsProvider";
 import Patientsvg from "../../../assets/imgs/DoctorPatient.svg";
+import Loading from "../../../Components/Loading/Loading";
+import API_ENDPOINT from "../../../utils/constants";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import axios from "axios";
 function PatientsList() {
   const [appointmentDetaiels, setAppointmentDetaiels] = useState(
     "No patients to display"
@@ -23,7 +28,41 @@ function PatientsList() {
     completedResponse,
     pendingResponse,
     cancelledResponse,
+    token
   } = useContext(ClinicsContext);
+  const [loading, setLoading] = useState(false); 
+  const [medications, setMedications] = useState([]);
+  const [diagnoses, setDiagnoses] = useState([]); 
+  const handleAI = async () => {
+    const authToken = localStorage.getItem("authToken");
+    try {
+      const params = {
+        patient_id: appointmentDetaiels.patient_id,
+        doctor_id: appointmentDetaiels.doctor_id,
+        appointment_id: appointmentDetaiels.id,
+      };
+      // Make the GET request using Axios with query parameters
+      const response = await axios.get(`${API_ENDPOINT}/api/doctor/patient-recommended-Profile/mobile`, {
+        headers: {
+          Authorization: `Bearer ${authToken}`, // Include the token in the Authorization header
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+        params: params,
+      });
+      console.log('Response:', response.data);
+      setMedications(response.data.recommend_medications)
+      setDiagnoses(response.data.recommend_diagnoses)
+    } catch (error) {
+      // Handle errors if the request fails
+      console.error('Error adding clinic:', error);
+      toast.error('Please Try Again', {
+        position: "bottom-right",
+        autoClose: 4000,
+      });
+      console.log("authToken", authToken);
+    }
+  };
 
   return (
     <div>
@@ -93,9 +132,7 @@ function PatientsList() {
                         <div
                           key={index}
                           className="row cursor"
-                          onClick={() =>
-                            setAppointmentDetaiels(response)
-                          }
+                          onClick={() => setAppointmentDetaiels(response)}
                         >
                           <p className="col-7 small"> {response.user_name}</p>
                           <p className="col-5 small ">
@@ -143,9 +180,7 @@ function PatientsList() {
                         <div
                           key={index}
                           className="row cursor"
-                          onClick={() =>
-                            setAppointmentDetaiels(response)
-                          }
+                          onClick={() => setAppointmentDetaiels(response)}
                         >
                           <p className="col-7 small"> {response.user_name}</p>
                           <p className="col-5 small ">
@@ -193,9 +228,7 @@ function PatientsList() {
                         <div
                           key={index}
                           className="row cursor"
-                          onClick={() =>
-                            setAppointmentDetaiels(response)
-                          }
+                          onClick={() => setAppointmentDetaiels(response)}
                         >
                           <p className="col-7 small"> {response.user_name}</p>
                           <p className="col-5 small ">
@@ -243,9 +276,7 @@ function PatientsList() {
                         <div
                           key={index}
                           className="row cursor"
-                          onClick={() =>
-                            setAppointmentDetaiels(response)
-                          }
+                          onClick={() => setAppointmentDetaiels(response)}
                         >
                           <p className="col-7 small"> {response.user_name}</p>
                           <p className="col-5 small ">
@@ -294,21 +325,86 @@ function PatientsList() {
         <div className="col-lg-6 add-clinc-sec ">
           {/* <h1>consultation</h1> */}
           <div className="mt-4 add-clinc-sec-content">
-            <div className="row mt-4 ms-1 ">          
+            <div className="row mt-4 ms-1 ">
               <img
-                src={appointmentDetaiels.profile_photo?appointmentDetaiels.profile_photo:Patientsvg}
+                src={
+                  appointmentDetaiels.profile_photo
+                    ? appointmentDetaiels.profile_photo
+                    : Patientsvg
+                }
                 alt="patient"
                 className="col-6 "
               />
-              <p className="col-6">  {appointmentDetaiels.user_name ? appointmentDetaiels.user_name : "No patients to display"} </p>
+              <p className="col-6">
+                {" "}
+                {appointmentDetaiels.user_name
+                  ? appointmentDetaiels.user_name
+                  : "No patients to display"}{" "}
+              </p>
             </div>
             <div className="cons-info mt-3"></div>
             <div className="mt-3 cons-info-p">
-              <p>Appointment Type: {appointmentDetaiels.appointment_type ? appointmentDetaiels.appointment_type : "No Info"} </p>
-              <p>Marital Status: {appointmentDetaiels.marital_status? appointmentDetaiels.marital_status : "No Info"} </p>
-              <p>Appointment day: {appointmentDetaiels.day? appointmentDetaiels.day : "No Info"} </p>
-              <p>Appointment Date: {appointmentDetaiels.date? appointmentDetaiels.date : "--/--/--"} </p>
-            
+              <p>
+                Appointment Type:{" "}
+                {appointmentDetaiels.appointment_type
+                  ? appointmentDetaiels.appointment_type
+                  : "No Info"}{" "}
+              </p>
+              <p>
+                Marital Status:{" "}
+                {appointmentDetaiels.marital_status
+                  ? appointmentDetaiels.marital_status
+                  : "No Info"}{" "}
+              </p>
+              <p>
+                Appointment day:{" "}
+                {appointmentDetaiels.day ? appointmentDetaiels.day : "No Info"}{" "}
+              </p>
+              <p>
+                Appointment Date:{" "}
+                {appointmentDetaiels.date
+                  ? appointmentDetaiels.date
+                  : "--/--/--"}{" "}
+              </p>
+              <div>
+                {" "}
+                {appointmentDetaiels.is_model_recommended ? (
+                  <div>
+                    <p>Review AI recommendations for diagnosis and treatment</p>
+                    <button
+                      className="border-0 active-button py-1 px-3 shadow  rounded-5"
+                      onClick={handleAI}
+                    >
+                      view
+                    </button>
+                    {diagnoses && (
+                      <div>
+                        <h6 className="mt-4">Diagnoses</h6>
+                        <hr />
+                        {diagnoses.map((diagnosis, index) => (
+                          <p className="small lh-1">
+                            {index + 1} - {diagnosis.Diagnoses}
+                          </p>
+                        ))}
+                      </div>
+                    )}
+
+                    {medications && (
+                      <div>
+                        <h6 >Medications</h6>
+                        <hr />
+                        {medications.map((medication, index) => (
+                          <p className="small lh-1">
+                            {index + 1} - {medication.Medication}
+                          </p>
+                        ))}
+                      </div>
+                    )}
+                  </div>
+                ) : (
+                  ""
+                )}
+              </div>
             </div>
           </div>
         </div>
